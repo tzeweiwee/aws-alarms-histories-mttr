d140c466769e8c1eb81e2648f4f86388fa0f2f2e
Tze Wei <tzeweiwee@gmail.com>
2018-01-02 17:33:32 +0800

    Update pull-alarms-history.sh

--- pull-alarms-history.sh
+++ pull-alarms-history.sh
@@ -1,12 +1,6 @@
 alarmsNames=(
-"cpp-review-ingest-service-js-prod-ReviewsDataProcessorQueueDepthAlarm-9EOD9W09F6BA"
-"CPP SG Prod PAPI 5XX Errors"
-"awseb-e-suxcwyqati-stack-ELB5XXBackendErrorCloudwatchAlarmCPPReact-M80GXSAJ8NHQ"
-"awseb-e-s6rcap2wp7-stack-ELB5XXBackendErrorCloudwatchAlarmCPPReact-1G2MLDLKT9ME4"
-"CPP SG Prod Reviews Action"
-"cpp-review-ingest-service-js-prod-ReviewsDataProcessorQueueOldestMessageAlarm-QCAD41DZQHCZ"
-"cpp-review-ingest-service-js-prod-PartialIngestSnsFailedNotificationDeliveryAlarm-XUHO3MVZ0KLS"
-"cpp-review-ingest-service-jobsdb-prod-APIGatewayIntegrationLatencyCloudWatchAlarm-J3AO0IVWX3VU"
+"Alarm 1"
+"Alarm 2"
 )
 
 output="alarmsHistory.json"
